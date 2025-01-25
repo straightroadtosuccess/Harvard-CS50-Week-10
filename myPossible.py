b1d680037fdd439c7aac29a898ed5f9661fdcf8f
straightroadtosuccess <straightroadtosuccess@hotmail.com>
2025-01-25 00:18:32 -0800

    Add files via upload

---
+++ myPossible.py
@@ -0,0 +1,10 @@
+# Possibility Calculation for amount of total possible combinations for Personal Identification Numbers (PIN) or other number possible combinations
+import math
+
+digits = int(input('What is the number of digits: '))
+
+possible = 10 ** digits
+
+print(' ')
+print('The amount of possible digits is: ' +str(possible))
+print(' ')
